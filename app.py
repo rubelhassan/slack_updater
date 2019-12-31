86116bc6ede871590917d6cdedf1eec50d5daa21
Rubel Hassan <rubelhassan@outlook.com>
2020-01-01 03:11:37 +0600

    Refactor app flow

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
-import argparse
-import os
-from app import GitMiner
-
-# TODO: remove when user preference is done
-parser = argparse.ArgumentParser()
-parser.add_argument('--paths', '-p', help="set git repository path")
-parser.add_argument('--branch', '-b', help="set git repository branch")
-args = parser.parse_args()
-
+from app import SlackUpdater, JiraParser
 
 if __name__ == '__main__':
-    if args.paths and os.path.isdir(args.paths):
-        miner = GitMiner(args.paths)
-        commits = miner.find_author_commits(10, 'rubel.hassan@dsinnovators.com', date='2019-12-10')
-        # commits = miner.find_author_commits(10, 'rubel.hassan@dsinnovators.com', branch='dev', date='2019-12-10')
-        for commit in commits:
-            print(commit)
-            print(commit.message, commit.author)
-            print(commit.authored_date)
-            print(commit.authored_datetime)
+    try:
+        slack_updater = SlackUpdater()
+        tickets = slack_updater.parse_tickets_from_commits(JiraParser())
+        slack_updater.update_to_slack_channel(tickets)
+    except Exception as e:
+        print("please configure settings.json")
+        print(e)
 
     print("Done Processing!")
